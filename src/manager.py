e5323fa6159efe62adc2d89ada8b4227ac8af4a3
Dexston123 <lambodawid@gmail.com>
2026-04-20 09:02:43 +0200

    Implementacja testu imiennego

--- src/manager.py
+++ src/manager.py
@@ -19,15 +19,13 @@ class Manager:
         self.bills = Bill.from_json_file(self.parameters.bills_json_path)
 
     def check_tenants_apartment_keys(self) -> bool:
-        for tenant in self.tenants.values():
-            if tenant.apartment not in self.apartments:
-                return False
-        return True
+        apartment_keys = set(self.apartments.keys())
+        tenant_apartments = (tenant.apartment for tenant in self.tenants.values())
+        return all(apt in apartment_keys for apt in tenant_apartments)
     
     def get_apartment(self, apartment_key: str) -> Apartment | None:
-        for apartment in self.apartments.values():
-            if apartment.key == apartment_key:
-                return apartment
+        if apartment_key in self.apartments:
+            return self.apartments[apartment_key]
         return None
 
     def get_apartment_costs(self, apartment_key: str, year: int = None, month: int = None) -> float | None:
